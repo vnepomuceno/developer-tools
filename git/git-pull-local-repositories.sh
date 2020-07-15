6c3baaed6d8a2898d29c0e1c8907c2ff285398fd
Valter Nepomuceno <valter.nep@gmail.com>
2020-07-15 21:55:39 +0100

    Update git repo sync shell script

--- git/git-pull-local-repositories.sh
+++ git/git-pull-local-repositories.sh
@@ -3,10 +3,14 @@ GREEN='\033[0;32m'
 RST=$(tput sgr0)
 bold=$(tput bold)
 normal=$(tput sgr0)
+CHECK='✅  '
+GLASS='🔎  '
+WAVE='👋  '
 BREAKER='------------------------------------------------------------------------------------------'
 
-for GIT_REPO in "$1"*;
-do
+pull_git_repositories()
+{
+  for GIT_REPO in *; do
     echo $BREAKER
     echo $(date) "==> Updating git repository '${bold}$GIT_REPO${normal}'"
     cd $GIT_REPO
@@ -14,4 +18,26 @@ do
     cd ..
     echo "\n\n" $(date) "${bold}==> ${GREEN}[OK]${RST} Finished pulling git repository"
     echo "$BREAKER\n"
+  done
+}
+
+echo $BREAKER
+echo "$GLASS Inspecting directory '$1'"
+echo $BREAKER
+echo "Repositories to be updated:"
+
+cd $1
+for GIT_REPO in *; do
+  echo "$CHECK $GIT_REPO"
+done
+
+echo $BREAKER
+
+while true; do
+    read -p "Continue to pull the listed repositories? [y/n] " yn
+    case $yn in
+        [Yy]* ) pull_git_repositories; break;;
+        [Nn]* ) echo "Bye! $WAVE"; exit;;
+        * ) echo "Please answer yes or no.";;
+    esac
 done
